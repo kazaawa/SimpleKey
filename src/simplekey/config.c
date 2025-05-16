13bdeae087df17b222222b8d5ddde17debef0b67
kazaawa <138601327+kazaawa@users.noreply.github.com>
2025-05-16 15:56:51 +0900

    config.c の更新

--- src/simplekey/config.c
+++ src/simplekey/config.c
@@ -9,8 +9,10 @@
 
 #define SPLIT_TRANSPORT_MIRROR
 #define SPLIT_LAYER_STATE_ENABLE
-
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
 #define SPLIT_OLED_ENABLE
+
 #define OLED_DISPLAY_128X64
 #define OLED_FONT_H "simplekey/lib/glcdfont.c"
 #define OLED_TIMEOUT 60000
