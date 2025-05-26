627b360eb34e4398455c7d151a04f8010da0ccc6
kazaawa <138601327+kazaawa@users.noreply.github.com>
2025-05-26 17:36:58 +0900

    config.h の更新

--- src/simplekey/config.h
+++ src/simplekey/config.h
@@ -1,1 +1,18 @@
+#pragma once
+#include "quantum.h"
+#define AUDIO_PIN D2
+
+#define SELECT_SOFT_SERIAL_SPEED 1
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+
+#define SPLIT_TRANSPORT_MIRROR
 #define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
+#define SPLIT_OLED_ENABLE
+
+#define OLED_DISPLAY_128X64
+#define OLED_UPDATE_INTERVAL 50
+#define OLED_FONT_H "simplekey/lib/glcdfont.c"
+#define OLED_TIMEOUT 60000
