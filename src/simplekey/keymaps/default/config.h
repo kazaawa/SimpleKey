ae7ac7df8139c5fd46c55c900ec9f0adf7cc04de
kazaawa <138601327+kazaawa@users.noreply.github.com>
2025-05-27 10:06:05 +0900

    config.h の更新

--- src/simplekey/keymaps/default/config.h
+++ src/simplekey/keymaps/default/config.h
@@ -1,10 +1,4 @@
 #pragma once
-#include "quantum.h"
-#define AUDIO_PIN D2
-
-#define SELECT_SOFT_SERIAL_SPEED 1
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
 #define SPLIT_TRANSPORT_MIRROR
 #define SPLIT_LAYER_STATE_ENABLE
