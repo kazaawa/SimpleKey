3aa0b75d939b3043b0c7d0fcba8533572f6ef454
kazaawa <138601327+kazaawa@users.noreply.github.com>
2025-06-12 01:43:25 +0900

    Update keymap.c

--- simplekey/keymaps/default/keymap.c
+++ simplekey/keymaps/default/keymap.c
@@ -78,11 +78,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef OLED_ENABLE
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+    oled_write_P(qmk_logo, false);
+}
+
+void oled_write_host_led_state(void) {
+    const led_t led_state = host_keyboard_led_state();
+    oled_write_P(PSTR("NumL:"), false);
+    oled_write_P(led_state.num_lock ? PSTR("on") : PSTR("- "), false);
+    oled_write_P(PSTR(" CapL:"), false);
+    oled_write_P(led_state.caps_lock ? PSTR("on") : PSTR("- "), false);
+    oled_write_P(PSTR(" SclL:"), false);
+    oled_write_ln_P(led_state.scroll_lock ? PSTR("on") : PSTR("-"), false);
+}
+
 bool oled_task_user(void) {
+
+    oled_write_ln_P(PSTR("Simple Key"), false);
+    
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
     switch (get_highest_layer(layer_state)) {
+		
         case _QWERTY:
             oled_write_P(PSTR("Default\n"), false);
             break;
@@ -97,7 +121,9 @@ bool oled_task_user(void) {
             oled_write_ln_P(PSTR("Undefined"), false);
     }
 
-    return false;
+        render_logo();  // Renders a static logo
+
+        return false;
 }
 
 
@@ -136,38 +162,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_master()) return OLED_ROTATION_180;
+    if (!is_keyboard_master()){
+ return OLED_ROTATION_180;
+}
     return rotation;
 }
 
-void oled_write_host_led_state(void) {
-    const led_t led_state = host_keyboard_led_state();
-    oled_write_P(PSTR("NumL:"), false);
-    oled_write_P(led_state.num_lock ? PSTR("on") : PSTR("- "), false);
-    oled_write_P(PSTR(" CapL:"), false);
-    oled_write_P(led_state.caps_lock ? PSTR("on") : PSTR("- "), false);
-    oled_write_P(PSTR(" SclL:"), false);
-    oled_write_ln_P(led_state.scroll_lock ? PSTR("on") : PSTR("-"), false);
-}
 
-bool oled_task_user(void) {
-    oled_write_host_led_state();
-    return false;
-}
 
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
-}
-
-bool oled_task_user(void) {
-    render_logo();
-    return false;
-}
 
 #endif
