fd43b341245671a1bdcb88ecd3f16f4e9d551e23
kazaawa <138601327+kazaawa@users.noreply.github.com>
2025-06-13 00:09:19 +0900

    Update keymap.c

--- simplekey/keymaps/default/keymap.c
+++ simplekey/keymaps/default/keymap.c
@@ -122,12 +122,14 @@ bool oled_task_user(void) {
     }
 
     // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+led_t led_state = host_keyboard_led_state();
+{
+    oled_write_P(led_state.num_lock ? PSTR("NL_ON ") : PSTR("N_OFF "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CP_ON ") : PSTR("C_OFF "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SR_ON ") : PSTR("S_OFF "), false);
+}
 	
-	oled_write_ln_P(PSTR("         Powered By"), false);
+	oled_write_ln_P(PSTR("   Powered By"), false);
     render_logo();  // Renders a static logo
 
         return false;
@@ -174,8 +176,5 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
     return rotation;
 }
-
-
-
 
 #endif
